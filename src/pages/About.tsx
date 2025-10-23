634e3d60ad1fbd2b061b2446b20f694b2b04c591
agmira <ag.mira@duocuc.cl>
2025-10-23 01:18:05 -0300

    añadí imagen nosotros

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ const About: React.FC = () => {
       <div className="row mb-5">
         <div className="col-lg-6 mb-4">
           <img 
-            src="https://via.placeholder.com/600x400?text=Nuestra+Tienda" 
+            src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1470" 
             alt="TechStore" 
             className="img-fluid rounded shadow"
           />
